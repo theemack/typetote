3cf4f3c45dea7627ffc673f01f8ab9866ba90cfd
Misha <Cromian@users.noreply.github.com>
2021-06-20 22:19:34 -0400

    Update Entity.php
    Fixed xml map typo

--- _app/Entity.php
+++ _app/Entity.php
@@ -382,11 +382,7 @@ class Entity
 
   // Used to create a static sitemap after content manifest is generated.
   public function createSiteMap($src) {
-    $xml_header = '<?xml version="1.0" encoding="UTF-8"?><urlset
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"></urlset>';
+    $xml_header = '<?xml?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>';
     $xml = new SimpleXMLElement($xml_header);
     
     // Add homepage
@@ -423,7 +419,7 @@ class Entity
   public function createRSS($manifest)
   {
     $site_data = $this->readDataFile(SiteInfo::getDataDir() . '/settings/site_info.json');
-    $rss_header = '<?xml version="1.0" encoding="UTF-8"?><rss></rss>';
+    $rss_header = '<?xml?><rss></rss>';
     $rss = new SimpleXMLElement($rss_header);
     $rss->addAttribute('version', '2.0');
     
